06884beb94e572c983715705486ca3c6a526bc10
aleksejpetrovec <neksomg123@gmail.com>
2022-09-12 16:13:07 +0300

    done :)

--- hw_class.py
+++ hw_class.py
@@ -7,7 +7,7 @@
 class SimpleClass:
 
     def __init__(self):
-        self.glasn = ['a', 'e', 'u', 'i', 'o']
+        self.glasn = ['a', 'e', 'u', 'i', 'o', 'y']
         self.empty_gl = []
         self.empty_sogl = []
         self.sum_chet = 0
@@ -21,9 +21,11 @@ class SimpleClass:
     def func1(self, some_string):
         if not some_string.isdigit():
             for i in some_string:
-                if i not in self.empty_gl and i in self.glasn:
+                if i == ' ':
+                    continue
+                elif i not in self.empty_gl and i in self.glasn:
                     self.empty_gl.append(i)
-                elif i not in self.empty_sogl:
+                elif i not in self.empty_sogl and i not in self.glasn:
                     self.empty_sogl.append(i)
                 else:
                     continue
@@ -33,16 +35,16 @@ class SimpleClass:
                 self.count_sogl += some_string.count(y)
             result = self.count_gl * self.count_sogl
             if result <= self.func2(some_string):
-                return self.empty_gl
+                return f'Гласные буквы в данной строке: {self.empty_gl}'
             else:
-                return self.empty_sogl
+                return f'Согласные буквы в данной строке: {self.empty_sogl}'
         else:
             for i in some_string:
                 if int(i) % 2 == 0:
                     self.sum_chet += int(i)
                 else:
                     continue
-            return self.sum_chet * self.func2(some_string)
+            return f'Произведение суммы нечетных цифр на длину числа: {self.sum_chet * self.func2(some_string)}'
 
 
 simple = SimpleClass()
